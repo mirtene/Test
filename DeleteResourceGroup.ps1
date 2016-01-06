7360843bf29741a1c0f3b84023df8b598ca417a7
Morten Eriksen <mirtenerik@gmail.com>
2016-01-06 15:54:24 +0100

    Created DeleteResourceGroup.ps1 in AzureAutomation.

---
+++ DeleteResourceGroup.ps1
@@ -0,0 +1,29 @@
+workflow DeleteResourceGroup
+{
+	Param
+	(
+		[parameter(Mandatory=$true)]
+        [String]$vnetResourceGroup
+	)
+	
+		#Authenticate Runbook to Subscription
+	Write-Output "Authenticating Runbook to Subscription.."
+		$CredentialAssetName = 'CredentialAssetName'
+		$Cred = GetAutomationPSCredential -Name $CredentialAssetName
+		if(!$Cred) 
+		{
+			Throw "Could not find an Automation Credential Asset named '$CredentialAssetName'. Make sure you have created one in this Automation Account."
+		}
+
+	#Connect to AzureRM Account
+	Write-Output "Connecting to AzureRM Account.."
+		$ARMAccount = Login-AzureRMAccount -Credential $Cred
+		if(!$ARMAccount) 
+		{
+			Throw "Could not authenticate AzureRM Account. Check username and password."
+		}
+	
+	#Delete Resource Group	
+	Write-Output "Deleting Resource Group '$vnetResourceGroup'.."
+		Remove-AzureRMResourceGroup -Name $vnetResourceGroup -Force
+}
