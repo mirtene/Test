7efdcc2bbe2d9ac526b048bdd2c93201784052f0
Morten Eriksen <mirtenerik@gmail.com>
2016-01-06 15:51:16 +0100

    Created StopEnvironment.ps1 in AzureAutomation.

---
+++ StopEnvironment.ps1
@@ -0,0 +1,46 @@
+workflow StopEnvironment
+{
+	#Parameters
+	Param (
+			[parameter(Mandatory=$true)]
+        	[String]$vnetResourceGroup	
+	)
+	
+	#Authenticate Runbook to Subscription
+	Write-Output "Authenticating Runbook to Subscription.."
+		$CredentialAssetName = 'CredentialAssetName'
+		$Cred = GetAutomationPSCredential -Name $CredentialAssetName
+		if(!$Cred) {
+			Throw "Could not find an Automation Credential Asset named '$CredentialAssetName'. Make sure you have created one in this Automation Account."
+		}
+
+	#Connect to AzureRM Account
+	Write-Output "Connecting to AzureRM Account.."
+		$ARMAccount = Login-AzureRMAccount -Credential $Cred
+		if(!$ARMAccount) {
+			Throw "Could not authenticate AzureRM Account. Check username and password."
+		}
+	
+	 # Get a list of Azure VMs
+	$vmList = Get-AzureRmVM -ResourceGroupName $Using:vnetResourceGroup
+    Write-Output "Number of Virtual Machines found in RG: [$($vmList.Count)] Name(s): [$($vmList.name)]"
+	foreach -parallel($inlineVm in $vmList)
+    {	
+		InlineScript
+		{
+			$vm = $using:inlineVm		
+		    # Stop all running VMs in ResourceGroup
+	        $vmStatus = Get-AzureRmVM -ResourceGroupName $vm.ResourceGroupName -Name $vm.Name -Status
+		
+           if($vmStatus.Statuses | where Code -match "PowerState/running") 
+           {
+                Write-Output "Stopping VM [$($vm.Name)]"
+                $vm | Stop-AzureRmVM -Force
+           }
+           else 
+		   {
+                Write-Output "VM [$($vm.Name)] is already deallocated!"
+           }
+		}
+	}          
+	Write-Output "All Running VMs were stopped!"
